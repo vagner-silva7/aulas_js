6ab3ea3930965fc1549bebfb92bb2b03530d36ec
vagner-silva7 <vagner.silva7@hotmail.com>
2022-04-06 17:04:00 -0300

    Revisão rapida

---
+++ js/02-revisao.js
@@ -0,0 +1,11 @@
+console.log("Revisão JS Senac!");
+
+let bebida = "Suco de laranja";
+
+const planeta = "Terra";
+
+// Moro no planeta terra
+
+console.log(`Moro no planeta ${planeta}`);
+
+console.log("Moro no planeta " +planeta);
