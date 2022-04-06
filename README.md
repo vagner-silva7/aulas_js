904059afd53b8e1609581c317c133c04dba08e78
vagner-silva7 <vagner.silva7@hotmail.com>
2022-04-06 16:26:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# aulas_js
+ Aulas de JavaScript
---
+++ js/01-sintaxe.js
@@ -0,0 +1,72 @@
+        // ctrl + ;       (comentario de linha unica)
+        /* alt + shft + A       (comentarios de multiplas linhas) */
+        
+        
+        /* Sempre que trabalhar com textos devemos colocar entre aspas simples '' ou aspas "" ou crase ¨  */
+
+
+        // Comando de saída de dados
+        console.log("Olá JavaScript!");
+
+        /* Variaveis e constantes, espaços identificados na memória para armazenar dados temporariamente */
+
+        // Variavel - let é igual a var (utilizado antigamente)
+        let ano = 2022;
+
+        // constante - valor não muda ou imutavel
+        const aluno = "Vagner";
+
+        console.log(ano);
+        console.log(aluno);
+        console.log(ano, aluno);
+
+        /* Operações matemáticas
+        + (adição)
+        - (subtração)
+        / (divisão)
+        * (multiplicação) */
+
+        let preco = 1500;
+        let desconto = 250;
+        let precoFinal = preco - desconto;
+
+        console.log(preco, desconto, precoFinal)
+
+        let produto = "Tv Led";
+
+        /* A TV LED CUSTAVA 1500 REAIS */
+
+        // 1a. forma (tradicional) -  concatenação
+
+        console.log("A "+produto+" custava "+preco+ " reais.");
+
+        // 2a. forma (moderna) - Template String/Literal
+
+        console.log(`A ${produto} custava ${preco} reais.`);
+
+        // Promp - comando de entrada para constante em teste
+
+        // const professor = prompt();
+
+        // console.log(professor)o)
+       
+
+        console.log(preco, desconto, precoFinal)
+
+  
+
+        /* A TV LED CUSTAVA 1500 REAIS */
+
+        // 1a. forma (tradicional) -  concatenação
+
+        console.log("A "+produto+" custava "+preco+ " reais.");
+
+        // 2a. forma (moderna) - Template String/Literal
+
+        console.log(`A ${produto} custava ${preco} reais.`);
+
+        // Promp - comando de entrada para constante em teste
+
+        // const professor = prompt();
+
+        // console.log(professor)
