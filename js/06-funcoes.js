6bbf110a82b9fec8526efc6ea47a3f1a1f7b0fc3
vagner-silva7 <vagner.silva7@hotmail.com>
2022-04-13 14:17:32 -0300

    Funções_13.04

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -22,19 +22,79 @@ console.log(`Ano: ${ano}`);
 
 linha ();
 
-/* Sintaxe tradicional, função com parametros ou argumentos */
-function soma (valor1, valor2) {
+/* Versão 1 - Sintaxe tradicional, função com parametros ou argumentos */
+/* function soma (valor1, valor2) {
     let total= valor1 + valor2;
     return total;
-}
+} */
 
-console.log ( soma(10,5));
 
-/* reaproveitando a função soma */
+// Versão 2: sintaxe Arrow funtion (função seta/flecha)
+/* const soma = (valor1, valor2) => {
+    let total = valor1 + valor2;
+    return total;
+}; */
+
+
+// Versão 3: sintaxe Arrow funtion (com retorno implicito)
+// retorno implicito remove {} e o return
+// Quando tem um parametro (ou numero). pode tirar o ()
+
+const soma = (valor1, valor2) => valor1 + valor2;
+
+
+console.log ( soma(100,50));
+
+/* reaproveitando a função soma2 */
 
 let resultado = soma (100, 50);
 console.log(`Total: ${resultado}`);
 
 if(resultado < 1000){
-    console.log("Abaixo da meta")
+    console.log(`Abaixo da meta`)
 }
+
+linha();
+
+
+// Versão 1: Tradicional
+/* function dobra(numero){
+    let resultado = numero * 2;
+    return resultado;
+} */
+
+
+// Versão 2: Tradicional
+/* function dobra(numero){
+    return resultado * 2;
+} */
+
+
+// Versão 3:Arrow funtion
+/* const dobra = (numero) => {
+    let resultado = numero * 2;
+    return resultado;
+}; */
+
+
+/* Versão 4: Arrow funtion (com retorno implicito)*/
+// retorno implicito remove {} e o return
+// Quando tem um parametro (ou numero). pode tirar o ()
+/* const dobra = (numero) => {
+    return numero * 2;
+}; */
+
+
+/* Versão 5: Arrow funtion (com retorno implicito) */
+// retorno implicito remove {} e o return
+// Quando tem um parametro (ou numero). pode tirar o ()
+const dobra = numero => numero * 2;
+
+
+console.log( dobra(10) );
+console.log( dobra(150) );
+
+linha();
+
+
+
